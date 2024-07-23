3366496128d1a0ab9a4c0dd7296c89139a9bce40
SudoBash <sudobashx@gmail.com>
2024-07-23 16:15:17 -0400

    Update CloudStricken.ps1

--- CloudStricken.ps1
+++ CloudStricken.ps1
@@ -84,7 +84,7 @@ fi
     $session = Connect-Hypervisor # Connect to your Hypervisor
 
     # Could manually define machines if you want more control:
-    # $machines = @("hostnamehere", "machine1", "machine2")
+    # $machines = @("vm-name", "vm1", "vm2")
     $machines = Get-VM -Name * # Get ALL Virtual Machines
     
     foreach ($machine in $machines) { # Loop through machines one at a time
